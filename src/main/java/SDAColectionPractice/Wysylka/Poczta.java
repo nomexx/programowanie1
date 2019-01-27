aa7bcd9be448fc541b48c38cef5ff30d46df8dc8
Maciej Caban <maciejcaban91@gmail.com>
2019-01-27 11:18:07 +0100

    dodaj methode

--- src/main/java/SDAColectionPractice/Wysylka/Poczta.java
+++ src/main/java/SDAColectionPractice/Wysylka/Poczta.java
@@ -1,5 +1,15 @@
 package SDAColectionPractice.Wysylka;
 
+import SDAColectionPractice.Produkt;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Poczta {
 
+    private List<Produkt> produkts= new ArrayList<>();
+
+    public void dodaj(Produkt produkt) {
+        produkts.add(produkt);
+    }
 }
