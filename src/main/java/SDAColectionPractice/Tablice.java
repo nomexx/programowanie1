ca80c1e754b467f6727bfd81dd552bff325b95c9
Maciej Caban <maciejcaban91@gmail.com>
2019-01-27 09:30:27 +0100

    sout add

--- src/main/java/SDAColectionPractice/Tablice.java
+++ src/main/java/SDAColectionPractice/Tablice.java
@@ -1,4 +1,22 @@
 package SDAColectionPractice;
 
+import java.util.Arrays;
+
 public class Tablice {
+    public static void main(String[] args) {
+
+
+        Produkt drzwi = new Produkt("Drzwi debowe", 30);
+        Produkt okno = new Produkt("okno drewniane", 15);
+
+
+        Produkt[] produkty = {drzwi, okno};
+
+        System.out.println("");
+        System.out.println(produkty);
+        System.out.println("");
+        System.out.println(Arrays.toString(produkty));
+
+    }
+
 }
