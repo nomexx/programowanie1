c2f1e424e47e640010b38d10f1839df3b32801d8
Maciej Caban <maciejcaban91@gmail.com>
2019-01-27 11:32:30 +0100

    zamien methode

--- src/main/java/SDAColectionPractice/Wysylka/Poczta.java
+++ src/main/java/SDAColectionPractice/Wysylka/Poczta.java
@@ -7,10 +7,21 @@ import java.util.List;
 
 public class Poczta {
 
-    private List<Produkt> produkts= new ArrayList<>();
+    private List<Produkt> produkts = new ArrayList<>();
 
     public void dodaj(Produkt produkt) {
         produkts.add(produkt);
     }
-    public  String toString(){return produkts.toString();}
+
+    public String toString() {
+        return produkts.toString();
+    }
+
+
+    public void zamien(Produkt staryProdukt, Produkt nowyProdukt) {
+        final int index = produkts.indexOf(staryProdukt);
+        produkts.set(index, nowyProdukt);
+    }
+
+
 }
--- src/main/java/SDAColectionPractice/Wysylka/PocztaTest.java
+++ src/main/java/SDAColectionPractice/Wysylka/PocztaTest.java
@@ -4,22 +4,28 @@ import SDAColectionPractice.Produkt;
 
 public class PocztaTest {
 
-        Produkt drzwi = new Produkt("Drzwi", 30);
-        Produkt okno = new Produkt("okno",15);
-        Produkt panel=new Produkt("panel",2);
-        Produkt deska = new Produkt("deska", 1);
+    Produkt drzwi = new Produkt("Drzwi", 30);
+    Produkt okno = new Produkt("okno", 15);
+    Produkt panel = new Produkt("panel", 2);
+    Produkt deska = new Produkt("deska", 1);
 
-        private Poczta poczta= new Poczta();
+    private Poczta poczta = new Poczta();
 
-        public void dodajProdukt(){
-            poczta.dodaj(okno);
-            poczta.dodaj(drzwi);
-            System.out.println("I test/ "+"\n"+poczta);
-        }
+    public void dodajProdukt() {
+        poczta.dodaj(okno);
+        poczta.dodaj(drzwi);
+        System.out.println("I test/ " + "\n" + poczta);
+    }
+
+    public void zamianaProduktu() {
+        poczta.zamien(okno, panel);
+        System.out.println("II test: " + "\n" + poczta);
+    }
 
     public static void main(String[] args) {
         PocztaTest pocztaTest = new PocztaTest();
         pocztaTest.dodajProdukt();
+        pocztaTest.zamianaProduktu();
     }
 
 }
